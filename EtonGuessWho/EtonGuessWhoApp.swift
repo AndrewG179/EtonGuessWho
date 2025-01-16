46182598b1e5dd0962073b95bf0331ac22b40e8a
Bromum <goryainov2487@gmail.com>
2025-01-16 10:21:55 +0000

    Initial Commit

---
+++ EtonGuessWho/EtonGuessWhoApp.swift
@@ -0,0 +1,17 @@
+//
+//  EtonGuessWhoApp.swift
+//  EtonGuessWho
+//
+//  Created by Андрей Горяинов on 16.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct EtonGuessWhoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ EtonGuessWhoTests/EtonGuessWhoTests.swift
@@ -0,0 +1,16 @@
+//
+//  EtonGuessWhoTests.swift
+//  EtonGuessWhoTests
+//
+//  Created by Андрей Горяинов on 16.01.2025.
+//
+
+import Testing
+
+struct EtonGuessWhoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
